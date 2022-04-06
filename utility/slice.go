3b5e1601e1d1b2be615a23a5094e209afff268cc
mooxiu <monetshaw@outlook.com>
2022-04-07 01:46:54 +0800

    fix

--- utility/slice.go
+++ utility/slice.go
@@ -1,7 +1,5 @@
 package utility
 
-import elements "music_a_gogo/structs"
-
 /*
 	If we have a slice []string{1, 2, 3}, left rolling 1 step will be []string{2, 3, 1}.
 */
@@ -18,12 +16,4 @@ func SliceLeftRolling(s []int, step int) []int {
 		}
 		return SliceLeftRolling(s, step)
 	}
-}
-
-func IntervalSum(intervals []elements.Interval) int {
-	sum := 0
-	for _, v := range intervals {
-		sum += int(v)
-	}
-	return sum
 }
